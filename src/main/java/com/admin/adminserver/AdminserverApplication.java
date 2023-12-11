4cc4bd5525aad353ba4552dafc2667ad07afefe4
nohheeyeon <heeyeon1213@gmail.com>
2023-12-11 13:11:15 +0900

    🐎Feat : "어드민 서버에 보안 설정 클래스 명시적 추가"

--- src/main/java/com/admin/adminserver/AdminserverApplication.java
+++ src/main/java/com/admin/adminserver/AdminserverApplication.java
@@ -1,11 +1,14 @@
 package com.admin.adminserver;
 
+import com.admin.adminserver.security.SecurityConfig;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableAdminServer
+@Import(SecurityConfig.class)
 public class AdminserverApplication {
 
 	public static void main(String[] args) {
